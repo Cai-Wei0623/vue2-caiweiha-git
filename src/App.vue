492065f6c1fbba8bfa387f83680e87e53fe7896d
Cai-Wei0623 <a0979650623@gmail.com>
2020-12-24 18:22:33 +0800

    刪掉home,about

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,6 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/week13">week13</router-link>
     </div>
     <router-view/>
   </div>
